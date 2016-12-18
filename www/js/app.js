1dfb1da21951baabd43308f36dee1c2b8e0abee5
tonimsc <tonimxc@gmail.com>
2016-12-18 22:15:05 +0100

    more nicer front

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
 // Ionic Starter App
-var base_url = "http://localhost:3000";
+//var base_url = "http://localhost:3000";
+var base_url = "http://192.168.1.35:3000";
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
